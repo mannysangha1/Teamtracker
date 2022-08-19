6e07b084b18cfe4b580bbd863a74ea8d5922aedc
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-08-19 15:31:37 -0700

    added set id and set email code

--- test/employee.test.js
+++ test/employee.test.js
@@ -10,3 +10,15 @@ test("Can set name via constructor arguments", () => {
     const e = new Employee(name);
     except(e.name).toBe(name);
     });
+
+test("Can set id by constructor argument", () => {
+    const testValue = 100;
+    const e = new Employee("Foo", testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test("Can set email via constructor argument", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("Foo", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
