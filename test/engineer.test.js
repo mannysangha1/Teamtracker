69d37b5c4cafde69fb4dc5556c46eb5fed98c021
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-08-20 15:43:43 -0700

    coded first part of code

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,3 +1,8 @@
 const Engineer = require("../lib/engineer");
 
-test
+test("Can set GitHub account via constructor", () =>{
+    const testValue = "GitHubUser";
+    const e = new Engineer("Foo", 1, "test@test.com", testValue);
+    expect(e.github).toBe(testValue);
+});
+
