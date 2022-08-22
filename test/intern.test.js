6eb91b6352e78afbe513d77ba6f8c29bdb34fb86
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-08-21 17:53:03 -0700

    started inter.test

---
+++ test/intern.test.js
@@ -0,0 +1,7 @@
+const Intern = require("../lib/intern");
+
+test("Can set school via constructor", () => {
+    const testValue = "UW";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.school).toBe(testValue);
+});
