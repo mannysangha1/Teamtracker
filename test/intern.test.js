3a265672200b04195f123eaebd151974520c1895
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-08-21 18:57:43 -0700

    finished third part

--- test/intern.test.js
+++ test/intern.test.js
@@ -11,3 +11,9 @@ test("getRole() should return \"Internt\"", () => {
     const e = new Intern("Foo", 1, "test@test.com", "UW");
     expect(e.getRole).toBe(testValue);
 });
+
+test("Can get school via getSchool()", () => {
+    const testValue = "UW";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.getSchool).toBe(testValue);
+});
