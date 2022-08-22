c87dc026b19961c825b1e043c42653583b29e9cb
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-08-22 08:57:07 -0700

    added html render

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ const inquirer = require ("inquirer");
 const path = require ("path");
 const fs = require ("fs");
 
+const HTMLrenderDone_DIR = path.resolve(__dirname, "htmlrenderdone");
+const htmlrenderdonePath = path.join(HTMLrenderDone_DIR, "generatedemployees.html");
