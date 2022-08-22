d5f130289be852894afad236cc63198300c45e9c
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-08-21 18:28:37 -0700

    finished second part

--- test/intern.test.js
+++ test/intern.test.js
@@ -9,5 +9,5 @@ test("Can set school via constructor", () => {
 test("getRole() should return \"Internt\"", () => {
     testValue = "Intern";
     const e = new Intern("Foo", 1, "test@test.com", "UW");
-    expect(e.getRole)
-})
+    expect(e.getRole).toBe(testValue);
+});
