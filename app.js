e5a82ff4a45422e2570e07b2f9f7393e5bce1bac
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-08-22 04:25:22 -0700

    started on app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const Manager = require ("./lib/Manager");
+const Engineer = require ("./lib/engineer");
+const Intern = require ("./lib/intern");
+const inquirer = require ("inquirer");
+const path = require ("path");
+const fs = require ("fs");
+
