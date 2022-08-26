88d7581ceb51a0ed16bd3545077d9fce6d7221a6
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-08-26 03:58:21 -0700

    fixed errors

--- test/employee.test.js
+++ test/employee.test.js
@@ -2,13 +2,13 @@ const Employee = require ("../lib/employee");
 
 test("can instantiate Employee instance", () => {
     const e = new Employee();
-    except(typeof(e)).toBe("object");
+    expect(typeof(e)).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
     const name = "Robert";
     const e = new Employee(name);
-    except(e.name).toBe(name);
+    expect(e.name).toBe(name);
     });
 
 test("Can set id by constructor argument", () => {
--- test/manager.test.js
+++ test/manager.test.js
@@ -1,21 +1,21 @@
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
-const { default: test } = require("test");
+const Manager = require("../lib/manager");
+const Employee = require("../lib/employee");
+
 
 test("Can set office number via constructor", () => {
     const testValue = 100;
     const e = new Manager("Foo", 1, "test@test.com", testValue);
-    expect(e.Officernumber()).toBE(testValue); 
+    expect(e.officeNumber).toBe(testValue); 
 });
 
-test("getRole() should return \"Manager\"", () => {
+test('getRole() should return "Manager"', () => {
     const testValue = "Manager";
     const e = new Manager("Foo", 1, "test@test.com", 100);
-    expect(e.getRole()).toBE(testValue);
+    expect(e.getRole()).toBe(testValue);
 });
 
-test("Can get office number via getOffice()", () => {
+test("Can get office number via getOfficeNumber()", () => {
     const testValue = 100;
     const e = new Manager("Foo", 1, "test@test.com", testValue);
-    expect(e.getOfficernumber()).toBE(testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
 });
